7f1d5be048410f5342fdc8bc7f9408579f2a4690
Zhoodar <zhoodar.abdikadyr@zensoft.io>
2018-02-07 19:45:25 +0600

    fig: the game handle algorithm, feat: alert about winner, restart the game

--- TicTacToe/app/src/main/java/kg/jedi/tictactoe/MainActivity.java
+++ TicTacToe/app/src/main/java/kg/jedi/tictactoe/MainActivity.java
@@ -1,10 +1,10 @@
 package kg.jedi.tictactoe;
 
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     private List<String> xsIds = new ArrayList<>();
     private List<String> osIds = new ArrayList<>();
 
-    final List<String> wPatterns = Arrays.asList("b1:b2:b3", "b1:b4:b7", "b1:b5:b9",
+    final List<String> winPatterns = Arrays.asList("b1:b2:b3", "b1:b4:b7", "b1:b5:b9",
             "b2:b5:b8", "b4:b5:b6", "b3:b5:b7", "b3:b6:b9", "b9:b8:b7");
 
     private Button b1, b2, b3, b4, b5, b6, b7, b8, b9;
@@ -49,7 +49,6 @@ public class MainActivity extends AppCompatActivity {
         b9 = (Button) findViewById(R.id.b9);
 
         TURN = F_PLAYER;
-
     }
 
     private void setListeners() {
@@ -64,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
         b9.setOnClickListener(eventListener);
     }
 
-    private void endGame(String v, String id) {
+    private void handleGame(String v, String id) {
         if (v.equals(markO)) {
             osIds.add(id);
         } else {
@@ -72,30 +71,33 @@ public class MainActivity extends AppCompatActivity {
         }
 
         if (xsIds.size() >= 3 && checkWinner(xsIds)) {
-            Toast.makeText(MainActivity.this, "Winner player X!", Toast.LENGTH_LONG).show();
+            finishGame("The player X is winner!");
         } else if (osIds.size() >= 3 && checkWinner(osIds)) {
-            Toast.makeText(MainActivity.this, "Winner player O!", Toast.LENGTH_LONG).show();
+            finishGame("The player O is winner!");
         }
     }
 
-    private void finishGame() {
+    private void finishGame(String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setMessage(message)
+                .setPositiveButton("New Game", (dialog, id) -> {
+                    dialog.dismiss();
+                    recreate();
+                });
 
+        builder.show();
     }
 
     private boolean checkWinner(List<String> pttS) {
-        State state = new State();
-        wPatterns.forEach(wP -> {
-            List p = Arrays.asList(wP.split(":"));
-            pttS.forEach(it -> {
-                state.setState(false);
-                if (p.contains(it)) {
-                    state.setState(true);
-                }
-            });
+        for (String wP : winPatterns) {
+            List<String> p = Arrays.asList(wP.split(":"));
 
-        });
+            if (pttS.containsAll(p)) {
+                return true;
+            }
+        }
 
-        return state.getState();
+        return false;
     }
 
     private String getXmlId(int id) {
@@ -123,23 +125,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private class State {
-
-        private Boolean state;
-
-        State() {
-            this.state = false;
-        }
-
-        Boolean getState() {
-            return state;
-        }
-        void setState(Boolean state) {
-            this.state = state;
-        }
-    }
-
-
     private class EvenListener implements View.OnClickListener {
 
         @Override
@@ -156,7 +141,7 @@ public class MainActivity extends AppCompatActivity {
                     button.setText(markO);
                 }
             }
-            endGame(button.getText().toString(), getXmlId(button.getId()));
+            handleGame(button.getText().toString(), getXmlId(button.getId()));
         }
     }
 
