b3f66ff361b58a77140c14148709aa5963e15fda
Zhoodar <zhoodar.abdikadyr@zensoft.io>
2018-02-28 18:53:41 +0600

    feat: RecyclerView usage

--- Forecast/app/src/main/java/kg/jedi/forecast/MainActivity.java
+++ Forecast/app/src/main/java/kg/jedi/forecast/MainActivity.java
@@ -1,17 +1,22 @@
 package kg.jedi.forecast;
 
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.net.URL;
 
+import kg.jedi.forecast.adapter.ForecastAdapter;
 import kg.jedi.forecast.data.ForecastPreferences;
 import kg.jedi.forecast.utilities.NetworkUtils;
 import kg.jedi.forecast.utilities.OpenWeatherJsonUtils;
@@ -19,22 +24,55 @@ import kg.jedi.forecast.utilities.OpenWeatherJsonUtils;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mWeatherTextView;
-
     private TextView mErrorMessageDisplay;
-
     private ProgressBar mLoadingIndicator;
+    private RecyclerView recyclerView;
+    private ForecastAdapter mForecastAdapter;
+    private EventListener eventListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mWeatherTextView = findViewById(R.id.tv_weather_data);
+        init();
+        loadWeatherData();
+    }
+
+    private void init() {
+        eventListener = new EventListener();
+        mForecastAdapter = new ForecastAdapter(eventListener);
+
         mErrorMessageDisplay = findViewById(R.id.tv_error_message_display);
         mLoadingIndicator = findViewById(R.id.pb_loading_indicator);
 
-        loadWeatherData();
+        LinearLayoutManager layoutManager =
+                new LinearLayoutManager(MainActivity.this, LinearLayoutManager.VERTICAL, false);
+
+        recyclerView = findViewById(R.id.recyclerview_forecast);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(mForecastAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.forecast, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_refresh) {
+            mForecastAdapter.setWeatherData(null);
+            loadWeatherData();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private void loadWeatherData() {
@@ -44,11 +82,11 @@ public class MainActivity extends AppCompatActivity {
 
     private void showWeatherDataView() {
         mErrorMessageDisplay.setVisibility(View.INVISIBLE);
-        mWeatherTextView.setVisibility(View.VISIBLE);
+        recyclerView.setVisibility(View.VISIBLE);
     }
 
     private void showErrorMessage() {
-        mWeatherTextView.setVisibility(View.INVISIBLE);
+        recyclerView.setVisibility(View.INVISIBLE);
         mErrorMessageDisplay.setVisibility(View.VISIBLE);
     }
 
@@ -90,32 +128,20 @@ public class MainActivity extends AppCompatActivity {
             mLoadingIndicator.setVisibility(View.INVISIBLE);
             if (weatherData != null) {
                 showWeatherDataView();
-                for (String weatherString : weatherData) {
-                    mWeatherTextView.append((weatherString) + "\n\n\n");
-                }
+                mForecastAdapter.setWeatherData(weatherData);
             } else {
                 showErrorMessage();
             }
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.forecast, menu);
-        return true;
-    }
+    private class EventListener implements ForecastAdapter.ForecastAdapterOnClickHandler {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.action_refresh) {
-            mWeatherTextView.setText("");
-            loadWeatherData();
-            return true;
+        @Override
+        public void onClick(String weatherForDay) {
+            Context context = MainActivity.this;
+            Toast.makeText(context, weatherForDay, Toast.LENGTH_SHORT)
+                    .show();
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
---
+++ Forecast/app/src/main/java/kg/jedi/forecast/adapter/ForecastAdapter.java
@@ -0,0 +1,74 @@
+package kg.jedi.forecast.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import kg.jedi.forecast.R;
+
+/**
+ * @author Jedi on 2/28/18.
+ */
+
+public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.ForecastAdapterViewHolder> {
+
+    private String[] mWeatherData;
+    private final ForecastAdapterOnClickHandler mClickHandler;
+
+    public ForecastAdapter(ForecastAdapterOnClickHandler clickHandler) {
+        mClickHandler = clickHandler;
+    }
+
+    public void setWeatherData(String[] weatherData) {
+        mWeatherData = weatherData;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public ForecastAdapterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        int layoutIdForListItem = R.layout.forecast_list_item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
+
+        View view = inflater.inflate(layoutIdForListItem, parent, shouldAttachToParentImmediately);
+        return new ForecastAdapterViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ForecastAdapterViewHolder forecastAdapterViewHolder, int position) {
+        String weatherForThisDay = mWeatherData[position];
+        forecastAdapterViewHolder.mWeatherTextView.setText(weatherForThisDay);
+    }
+
+    @Override
+    public int getItemCount() {
+        if (null == mWeatherData) return 0;
+        return mWeatherData.length;
+    }
+
+    public class ForecastAdapterViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        public final TextView mWeatherTextView;
+
+        public ForecastAdapterViewHolder(View view) {
+            super(view);
+            mWeatherTextView = (TextView) view.findViewById(R.id.tv_weather_data);
+            view.setOnClickListener(this);
+        }
+
+
+        @Override
+        public void onClick(View v) {
+            int adapterPosition = getAdapterPosition();
+            String weatherForDay = mWeatherData[adapterPosition];
+            mClickHandler.onClick(weatherForDay);
+        }
+    }
+
+    public interface ForecastAdapterOnClickHandler {
+        void onClick(String weatherForDay);
+    }
+}
