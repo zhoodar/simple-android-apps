4ddade0cf9b9f4a2446bb148133686ac3303ffb8
zhoodar <joodar1993>
2018-04-22 14:55:45 +0600

    feat: layout for single expense, redesign

--- JFTracker/app/src/main/java/kg/jedi/jftracker/MainActivity.kt
+++ JFTracker/app/src/main/java/kg/jedi/jftracker/MainActivity.kt
@@ -2,7 +2,6 @@ package kg.jedi.jftracker
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.support.v7.app.ActionBar
 import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
 import kg.jedi.jftracker.fragment.HistoryFragment
@@ -13,14 +12,10 @@ import kotlinx.android.synthetic.main.main_activity.*
 
 class MainActivity : AppCompatActivity() {
 
-    private var toolbar: ActionBar? =null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
 
-        toolbar = supportActionBar
-        toolbar!!.title = "Home"
         loadFragment(HomeFragment())
 
         navigation.setOnNavigationItemSelectedListener { handleOnNavigationItemSelected(it) }
@@ -30,19 +25,16 @@ class MainActivity : AppCompatActivity() {
         val fragment: Fragment
         when (item.itemId) {
             R.id.nav_home -> {
-                toolbar!!.title = "Home"
                 fragment = HomeFragment()
                 loadFragment(fragment)
                 return true
             }
             R.id.nav_history -> {
-                toolbar!!.title = "Archive"
                 fragment = HistoryFragment()
                 loadFragment(fragment)
                 return true
             }
             R.id.nav_setting -> {
-                toolbar!!.title = "Setting"
                 fragment = SettingFragment()
                 loadFragment(fragment)
                 return true
